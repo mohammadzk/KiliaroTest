434a96920e9b47e9a3d2f3dde78d37d3ebe2e82e
Mohammad Khazaee <m.khazaee@sabaidea.com>
2022-12-09 17:08:59 +0330

    Initial Commit

---
+++ KiliaroTest/KiliaroTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  KiliaroTestApp.swift
+//  KiliaroTest
+//
+//  Created by Mohammad khazaee on 12/9/22.
+//
+
+import SwiftUI
+
+@main
+struct KiliaroTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
